f62c9336b1ab2b036507d368d652fd9ed882f569
Evert de la Ferté <evertferte@hotmail.com>
2020-03-26 11:46:23 +0100

    Finished base game | Improved tile visuals

--- src/main/java/reversi/GUI.java
+++ src/main/java/reversi/GUI.java
@@ -13,16 +13,20 @@ import javafx.stage.Stage;
 import java.util.ArrayList;
 
 public class GUI extends Application {
-    private int width = 400;
-    private int height = 400;
-    private int mapSize = 8;
+    private static final String emptyId = "e";
+    private static final String blackId = "b";
+    private static final String whiteId = "w";
+    private static final int mapSize = 8;
+    
+    private int width = 800;
+    private int height = 800;
     private ArrayList<ArrayList<Button>> map = new ArrayList<>();
 
-    private Image tileEmpty = new Image(getClass().getResourceAsStream("/images/tile_empty.png"));
-    private Image tileWhite = new Image(getClass().getResourceAsStream("/images/tile_white.png"));
-    private Image tileBlack = new Image(getClass().getResourceAsStream("/images/tile_black.png"));
+    private Image tileEmpty = new Image(getClass().getResourceAsStream("/images/reversi/tile_empty_fade.png"));
+    private Image tileWhite = new Image(getClass().getResourceAsStream("/images/reversi/tile_white_0.png"));
+    private Image tileBlack = new Image(getClass().getResourceAsStream("/images/reversi/tile_black_0.png"));
 
-    private boolean turn = true;
+    private boolean turn = false;
 
     public GUI() {
         // ...
@@ -37,7 +41,7 @@ public class GUI extends Application {
             for (int x = 0; x < mapSize; x++) {
                 Button btn = new Button();
                 map.get(y).add(btn);
-                map.get(y).get(x).setId("empty");
+                map.get(y).get(x).setId(emptyId);
 
                 btn.setMinSize((float)width / (float)mapSize, (float)height / (float)mapSize);
 
@@ -48,14 +52,27 @@ public class GUI extends Application {
 
                 btn.setOnAction(new ButtonHandler(x, y, this));
 
-                grid.add(btn, y, x);
+                grid.add(btn, x, y);
             }
         }
-
+        
         clickPosition(3, 3, true);
         clickPosition(3, 4, true);
         clickPosition(4, 4, true);
         clickPosition(4, 3, true);
+    
+        System.out.println(turn ? "white's turn" : "black's turn");
+        
+//        //TEMP
+//        clickPosition(5, 0, true);
+//        clickPosition(6, 0, true);
+//
+//        clickPosition(6, 1, true);
+//
+//        clickPosition(7, 1, true);
+//        clickPosition(7, 2, true);
+//        turn = !turn;
+        //TEMP
 
         Scene scene = new Scene(grid, width, height);
 
@@ -69,57 +86,128 @@ public class GUI extends Application {
     }
 
     private void clickPosition(int xPos, int yPos, boolean forceClick) {
+//        if (!forceClick) System.out.println("click("+xPos+", "+yPos+")");
+        
         // Check if we place the new tile to an existing tile
         Button current = map.get(yPos).get(xPos);
         String currentId = current.getId();
 
         // Return if the current tile is not empty
-        if (currentId != "empty")
+        if (!currentId.equals(emptyId))
             return;
 
         // ...
-        String tileId = turn ? "tileWhite" : "tileBlack";
         boolean valid = false;
-
-        if (forceClick)
+        
+        if (forceClick) {
+            // Place the new tile
+            ImageView img = new ImageView(turn ? tileWhite : tileBlack);
+            img.setFitWidth((float)width / (float)mapSize);
+            img.setFitHeight((float)height / (float)mapSize);
+    
+            current.setGraphic(img);
+            current.setId(turn ? whiteId : blackId);
+            
             valid = true;
+        }
         else {
-            if (yPos + 1 < mapSize) {
-                String topId = map.get(yPos + 1).get(xPos).getId();
-                if (topId == "tileWhite" || topId == "tileBlack") valid = true;
-            }
-            if (yPos - 1 >= 0) {
-                String botId = map.get(yPos - 1).get(xPos).getId();
-                if (botId == "tileWhite" || botId == "tileBlack") valid = true;
-            }
-            if (xPos + 1 < mapSize) {
-                String rightId = map.get(yPos).get(xPos + 1).getId();
-                if (rightId == "tileWhite" || rightId == "tileBlack") valid = true;
-            }
-            if (xPos - 1 >= 0) {
-                String leftId = map.get(yPos).get(xPos - 1).getId();
-                if (leftId == "tileWhite" || leftId == "tileBlack") valid = true;
+            valid = checkDirection(xPos, yPos,  0, -1);              // Up
+            valid = checkDirection(xPos, yPos,  0,  1) || valid;     // Down
+            valid = checkDirection(xPos, yPos, -1,  0) || valid;     // Left
+            valid = checkDirection(xPos, yPos,  1,  0) || valid;     // Right
+            
+            valid = checkDirection(xPos, yPos, -1, -1) || valid;     // Up left
+            valid = checkDirection(xPos, yPos,  1, -1) || valid;     // Up right
+            valid = checkDirection(xPos, yPos, -1,  1) || valid;     // Down left
+            valid = checkDirection(xPos, yPos,  1,  1) || valid;     // Down right
+            
+            if (valid) {
+                // Place the new tile
+                ImageView img = new ImageView(turn ? tileWhite : tileBlack);
+                img.setFitWidth((float)width / (float)mapSize);
+                img.setFitHeight((float)height / (float)mapSize);
+        
+                current.setGraphic(img);
+                current.setId(turn ? whiteId : blackId);
             }
         }
-
-        if (!valid) { return; }
-
-        // Place the new tile
-        ImageView img2 = new ImageView(turn ? tileWhite : tileBlack);
-        img2.setFitWidth((float)width / (float)mapSize);
-        img2.setFitHeight((float)height / (float)mapSize);
-
-        current.setGraphic(img2);
-        current.setId(turn ? "tileWhite" : "tileBlack");
-
-        if (!forceClick)
-            checkMove(xPos, yPos);
-
-        turn = !turn;
+        
+        // Swap the turn, only if this turn was valid
+        turn = valid != turn;
+    
+        if (!forceClick) System.out.println(turn ? "white's turn" : "black's turn");
+    }
+    
+    private boolean checkDirection(int xPos, int yPos, int xDir, int yDir) {
+        if (xDir == 0 && yDir == 0) return false;
+        
+        boolean valid = false, oppositeFound = false;
+        String currentPlayerId  = turn ? whiteId : blackId;
+        String oppositePlayerId = turn ? blackId : whiteId;
+        
+        int newX = xPos + xDir;
+        int newY = yPos + yDir;
+        
+        // Loop while the new position is still a valid position in the map
+        int i = 0;
+        while (newX > 0 && newX < mapSize - 1 && newY > 0 && newY < mapSize - 1) {
+//        while (newX >= 0 && newX < mapSize && newY >= 0 && newY < mapSize) {
+            i++;
+            
+            newX = xPos + xDir * i;
+            newY = yPos + yDir * i;
+            
+            // Get the ID of the current tile
+            String id = map.get(newY).get(newX).getId();
+            
+//            System.out.println("checking pos (" + newX + ", " + newY + ")");
+    
+            // If empty, stop
+            if (id.equals(emptyId)) break;
+    
+            // If opposite player, continue
+            if (id.equals(oppositePlayerId)) {
+                oppositeFound = true;
+                continue;
+            }
+    
+            // If current player, break and make valid only if an opposite tile was found
+            if (id.equals(currentPlayerId)) {
+                if (oppositeFound)
+                    valid = true;
+                break;
+            }
+        }
+    
+//        System.out.println(valid ? "valid" : "not valid");
+    
+        if (valid) {
+            System.out.println("valid found, replacing adjacent tiles ("+i+" tile(s) )");
+            for (int j = 1; j < i; j++) {
+                int x = xPos + xDir * j;
+                int y = yPos + yDir * j;
+                
+                String id = map.get(y).get(x).getId();
+                System.out.println("replacing ("+x+", "+y+") (id: "+id+")");
+                
+                if (!id.equals(currentPlayerId)) {
+                    // convert tile to the same tile as the current player
+                    ImageView img = new ImageView(turn ? tileWhite : tileBlack);
+                    img.setFitWidth((float)width / (float)mapSize);
+                    img.setFitHeight((float)height / (float)mapSize);
+                    map.get(y).get(x).setGraphic(img);
+                    map.get(y).get(x).setId(currentPlayerId);
+                }
+                else
+                    break;
+            }
+        }
+        
+        return valid;
     }
 
     private void checkMove(int xPos, int yPos) {
-        String tileId = !turn ? "tileWhite" : "tileBlack";
+        String tileId = !turn ? whiteId : blackId;
 
         // check directions (top, bot, left, right, diagonal) for black or white tile
             // skip if the direction is of the same color as the current color
@@ -135,7 +223,7 @@ public class GUI extends Application {
                 if (xPos + x < 0 || xPos + x >= mapSize)
                     continue;
 
-                if (map.get(yPos + y).get(xPos + x).getId() == tileId)
+                if (map.get(yPos + y).get(xPos + x).getId().equals(tileId))
                     directions.add(new Vector2(x, y));
             }
         }
